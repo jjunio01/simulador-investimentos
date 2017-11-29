f1325e2db1094c3fb03e091e0de690c4611a00b7
José Junio <junio.araujo01@yahoo.com>
2017-11-29 00:16:58 -0300

    Criado package para testes unitários com Junit

---
+++ src/test/java/com/github/jjunio01/simulador/investimentos/poupanca/Poupanca.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.jjunio01.simulador.investimentos.poupanca;
+
+import com.github.jjunio01.simulador.investimentos.controller.ControllerInvestimento;
+import com.github.jjunio01.simulador.investimentos.model.InvestPoupanca;
+import com.github.jjunio01.simulador.investimentos.util.ErroSistema;
+import org.junit.Test;
+
+/**
+ *
+ * @author Jose Junio
+ */
+public class Poupanca {
+
+    @Test
+    public void testarRendimentoPoupancaNaoVazio() throws ErroSistema {
+        InvestPoupanca poupanca = new InvestPoupanca();
+        poupanca.setPeriodo(30);
+        poupanca.setValor(200);
+        ControllerInvestimento controller = new ControllerInvestimento();
+        controller.setPoupanca(poupanca);
+        controller.calcularRendimentos(poupanca);
+        System.out.println(controller.getPoupanca().getRendimentos());
+    }
+
+    @Test
+    public void testarValorRendimentoPoupanca() {
+
+    }
+
+}
