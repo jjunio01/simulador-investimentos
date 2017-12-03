4a281310696fb79ab5a466e21978709acb0545b9
José Junio <junio.araujo01@yahoo.com>
2017-12-03 15:59:29 -0300

    Criado teste para calcular valor atualizado da poupança

--- src/test/java/com/github/jjunio01/simulador/investimentos/InvestLci.java
+++ src/test/java/com/github/jjunio01/simulador/investimentos/InvestLci.java
@@ -5,6 +5,9 @@
  */
 package com.github.jjunio01.simulador.investimentos;
 
+import com.github.jjunio01.simulador.investimentos.model.InvestLCI;
+import java.math.BigDecimal;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -15,6 +18,16 @@ public class InvestLci {
 
     @Test
     public void testarValorAtualizadoLci() {
+
+        InvestLCI lci = new InvestLCI();
+        lci.setPeriodo(90);
+        lci.setValor(new BigDecimal("1000"));
+        lci.calcularRendimentos();
+        BigDecimal valor = new BigDecimal("1000");
+        //0.0053(Taxa Adicional) + 0.0000903(TR)
+        BigDecimal taxa = new BigDecimal("0.0000903").add(new BigDecimal("0.0053"));
+        BigDecimal resultadoTeste = valor.multiply(taxa).add(valor);
+        Assert.assertEquals(lci.getValorAtualizado(), resultadoTeste);
 
     }
 
